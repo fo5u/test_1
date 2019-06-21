8b31ed332b0e5e479bdd5fbae5609ea6d4c9f48c
testers <testers@itstep.kharkov.ua>
2019-06-21 19:35:49 +0300

    new meth

---
+++ Sesson_code.py
@@ -0,0 +1,24 @@
+
+zima = ["December ","January","February",12,1,2]
+vesna = ["March","April","May",3,4,5 ]
+leto = ["June","Juli","August",6,7,8 ]
+osen = ["September","October","November",9,10,11]
+
+
+
+user_input = input("Vvedite nazvanie mesyaca: ")
+
+def sesson():
+    if user_input in zima:
+        print("Sesson Zima")
+    elif user_input in vesna:
+        print("Sesson Vesna")
+    elif user_input in leto:
+        print("Sesson Leto")
+    elif user_input in osen:
+        print("Sesson Osen'")
+    else:
+        print("Error")
+
+print sesson()
+
